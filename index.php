6672898d81f1d27e125a154021bbb76268cf34aa
Zhe Wu <zhewu@sysom.cn>
2015-10-24 15:34:22 +0800

    index.php: write the login input field

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <html>
 	<head>
 		<title>登陆</title>
+	<h1>
+		数字化校园智能放学系统
+	</h1>
 	</head>
 
 	<body>
 		<?php
-			echo "Hello PHP!";
-
 			$mysqli = new mysqli("localhost", "root", "", "school_over_system");
 			if (mysqli_connect_errno()) {
 				printf("Connect failed %s\n", mysqli_connect_errno());
@@ -26,5 +27,23 @@
 
 			$mysqli->close();
 		?>
+
+		<form name="login" action="login.php" method="post">
+			学校：
+			<select name="school">
+				<option value="diaozhuang">常州雕庄中心小学</option>
+			</select>
+			<br/>
+			姓名：
+			<input type="text" name="name"/>
+			<br/>
+			密码：
+			<input type="text" name="password"/>
+			<br/>
+			<input type="radio" name="role" value="teacher"/>老师
+			<input type="radio" name="role" value="parent"/>家长
+			<br/>
+			<input type="submit" value="登陆"/>
+		</form>
 	</body>
 </html>
