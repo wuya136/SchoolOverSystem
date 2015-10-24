7ed820d85ae585bd4a0e0868049c3727322333c2
Zhe Wu <zhewu@sysom.cn>
2015-10-24 12:34:37 +0800

    index: Test mysql database

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
 	<body>
 		<?php
 			echo "Hello PHP!";
+
+			$link = mysql_connect("localhost", "root", "");
+			if ($link)
+				die("Could not connect:" . mysql_error());
+			echo "Connected Successfully";
+			mysql_close($link);
 		?>
 	</body>
 </html>
