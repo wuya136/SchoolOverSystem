50d67e4e04238010d1f8ae00b426c606068cc581
Zhe Wu <zhewu@sysom.cn>
2015-10-24 15:45:17 +0800

    index: Add more school list

--- index.php
+++ index.php
@@ -31,7 +31,18 @@
 		<form name="login" action="login.php" method="post">
 			学校：
 			<select name="school">
-				<option value="diaozhuang">常州雕庄中心小学</option>
+				<option value="diaozhuang">常州市雕庄中心小学</option>
+				<option value="hongjing">常州市虹景小学</option>
+				<option value="boailu">常州市博爱路小学</option>
+				<option value="jiefanglu">常州市解放路小学</option>
+				<option value="juqianjie">常州市局前街小学</option>
+				<option value="qingliang">常州市清凉小学</option>
+				<option value="beihuanlu">常州市北环路小学</option>
+				<option value="dierxincun">常州市第二新村小学</option>
+				<option value="disanxincun">常州市第三新村小学</option>
+				<option value="chaoyangqiao">常州市朝阳桥小学</option>
+				<option value="chaoyangxincun">常州市朝阳新村小学</option>
+				<option value="beijiao">常州市北郊小学</option>
 			</select>
 			<br/>
 			姓名：
