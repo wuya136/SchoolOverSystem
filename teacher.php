f5d7df4e5d2b1b7b725e9a25c4176a4e1bd32c79
Zhe Wu <zhewu@sysom.cn>
2015-10-24 17:01:25 +0800

    techer: Add class input

--- teacher.php
+++ teacher.php
@@ -1,5 +1,38 @@
+<html>
+
+	<head>
+		<title>老师放学</title>
+	</head>
+	<h1>
+<?php
+printf("%s\n", $TEACHER_NAME);
+?>
+		老师，你好！
+	</h1>
+
+	<body>
+
+	<form name="students" action="teacher.php" method="post">
+		班级：
+		<select name="class">
+<?php
+$query = "select name from class";
+if ($result = $mysqli->query($query)) {
+	while ($obj = $result->fetch_object()) {
+		$CLASS_ID = $obj->class_id;
+		$CLASS_NAME = $obj->name;
+		$htmlcode = "<option value='" . $CLASS_ID . "'>" . $CLASS_NAME . "</option>";
+		printf("%s\n", $htmlcode);
+	}
+}
+?>
+		</select>
+
+
+	</form>
 <?php
 
-echo "$TEACHER_ID, $TEACHER_NAME";
-
 ?>
+	</body>
+
+</html>
