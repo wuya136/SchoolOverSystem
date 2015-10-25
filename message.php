47ea642053d06089b85b59b641a94383495a8386
Zhe Wu <zhewu@sysom.cn>
2015-10-25 16:44:42 +0800

    message: Add message_to_parent support

--- message.php
+++ message.php
@@ -62,8 +62,23 @@ if (isset($_POST['message_to_all'])) {
 	$author_type = 1;
 	$author_id = $_SESSION['teacher_id'];
 	$createtime = date("Y-m-d H:i:s");
-
 	$STUDENT_ID = $_POST['keeped_student'];
+
+	$start_time = substr($_SESSION['overtime'], 0, 10) . " 00:00:00";
+	$end_time = substr($_SESSION['overtime'], 0, 10) . " 23:59:59";
+	$query = "select schoolover_id from schoolover where student_id='" . $STUDENT_ID . "' and overtime>'" . $start_time . "' and overtime<'" . $end_time . "';" ;
+	$result = $mysqli_message->query($query);
+	$obj = $result2->fetch_object();
+	$SCHOOLOVER_ID = $obj->schoolover_id;
+
+	$insert = "insert into message (schoolover_id, author_type, author_id, content, type, createtime) VALUES" . "($SCHOOLOVER_ID, $author_type, $author_id, \"$message\", $message_type, \"$createtime\");";
+	/*
+	 * Insert message into database
+	 */
+	if ($mysqli_message->query($insert))
+		printf("Update message table successfully\n");
+	else
+		printf("%s failed to execute\n" ,$insert);
 }
 
 
