a4f549a0325be684919aba0c511b1f1442792f6a
Zhe Wu <zhewu@sysom.cn>
2015-10-25 21:48:42 +0800

    parent: Fix format

--- parent.php
+++ parent.php
@@ -109,6 +109,7 @@ if ($result = $mysqli_parent->query($query)) {
 $htmlcode .= "</textarea>";
 printf("%s\n", $htmlcode);
 ?>
+		</br>
 		<input type="text" name="message_to_teacher"/>
 		<input type="submit" value="发送"/>
 		</form>
